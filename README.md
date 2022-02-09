39fda85b541dbc378329ef79ae8159e2e8fb667e
SamHuang <SamHuang@supermicro.com.tw>
2022-02-10 02:57:21 +0800

    TwoSum

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 | # | Title | Tags | Solution | Hint | Complexity |
 |---| ----- | -------- | ---------- | ---------- | ---------- |
-|1| [Two Sum](https://leetcode.com/problems/two-sum/) | Array, Eazy | [C++](./solution/cpp/TwoSum/TwoSum.cpp) | Use unordered_map | O(n) |
+|1| [Two Sum](https://leetcode.com/problems/two-sum/) | Array, Eazy | [C++](./source/cpp/TwoSum/TwoSum.cpp) | Use unordered_map | O(n) |
---
+++ source/cpp/TwoSum/TwoSum.cpp
@@ -0,0 +1,41 @@
+/**********************************************************************************
+*
+* Two Sum
+* Source: https://leetcode.com/problems/two-sum/
+* @author Sam Huang
+* @version 09/15/2021
+*
+*
+* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+* You may assume that each input would have exactly one solution, and you may not use the same element twice.
+* You can return the answer in any order.
+*
+* Example:
+*
+* Input: nums = [2,7,11,15], target = 9
+* Output: [0,1]
+* Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+*
+*
+**********************************************************************************/
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        int need_num;
+        unordered_map<int, int> nums_map;
+        for(int i=0; i<nums.size(); i++){
+            need_num = target -nums[i];
+            if(nums_map.find(need_num) != nums_map.end()){
+                ans.push_back(nums_map[need_num]);
+                ans.push_back(i);
+                return ans;
+            } else {
+                nums_map[nums[i]] = i;
+            }
+        }
+        return ans;
+    }
+};
